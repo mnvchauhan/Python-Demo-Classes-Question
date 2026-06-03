6ac9ce08c1c3e78064d883f75d77fe81c82649d2
mnvchauhan <manavchauhan0442@gmail.com>
2026-06-03 12:06:48 +0530

    first commit

---
+++ class.py
@@ -0,0 +1,28 @@
+# print("hello",end="\n")
+# print("hello")
+
+# print("19","may","2026",sep="+")
+# print("19","may","2026")
+
+
+
+#ye code do number ko add kr rha hain.....
+
+
+
+#erjgilweg
+#etlfjwoe
+
+
+''',ffnesdf,mgvbsdmbgvrwsdbfvjhbsdfjvbjxcbvnsdbmnebsdmcn besmndfbcjembvjmfdvbjm dbfcjv brjfdbvjfbdcvmbfdjhgbvrd'v
+;fcgjvr;'df
+gvrfg;vrf
+gvrt
+dfgvrt
+fdBv
+rgfb
+tgf
+btyg
+fbt
+gfb 
+gfv'''
